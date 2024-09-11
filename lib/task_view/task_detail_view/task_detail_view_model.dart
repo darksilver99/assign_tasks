d3339da70bc500229606639fcc0bc7ac78610b64
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2024-09-11 10:57:57 +0000

    Updating to latest FlutterFlow output.

--- lib/task_view/task_detail_view/task_detail_view_model.dart
+++ lib/task_view/task_detail_view/task_detail_view_model.dart
@@ -1,6 +1,7 @@
 import '/auth/firebase_auth/auth_util.dart';
 import '/backend/backend.dart';
 import '/component/info_custom_view/info_custom_view_widget.dart';
+import '/flutter_flow/flutter_flow_expanded_image_view.dart';
 import '/flutter_flow/flutter_flow_theme.dart';
 import '/flutter_flow/flutter_flow_util.dart';
 import '/flutter_flow/flutter_flow_widgets.dart';
@@ -14,6 +15,7 @@ import 'package:collection/collection.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/scheduler.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:page_transition/page_transition.dart';
 import 'package:provider/provider.dart';
 
 class TaskDetailViewModel extends FlutterFlowModel<TaskDetailViewWidget> {
