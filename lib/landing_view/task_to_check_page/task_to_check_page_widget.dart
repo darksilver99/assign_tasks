cfb53e719a10f4c26c26c050b49d798c5a2767e7
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2024-09-12 03:48:57 +0000

    Updating to latest FlutterFlow output.

--- lib/landing_view/task_to_check_page/task_to_check_page_widget.dart
+++ lib/landing_view/task_to_check_page/task_to_check_page_widget.dart
@@ -1,4 +1,5 @@
 import '/backend/backend.dart';
+import '/component/no_data_view/no_data_view_widget.dart';
 import '/customer_view/create_customer_view/create_customer_view_widget.dart';
 import '/flutter_flow/flutter_flow_theme.dart';
 import '/flutter_flow/flutter_flow_util.dart';
@@ -143,7 +144,8 @@ class _TaskToCheckPageWidgetState extends State<TaskToCheckPageWidget> {
                               ),
                             ),
                           ),
-
+                          noItemsFoundIndicatorBuilder: (_) =>
+                              NoDataViewWidget(),
                           itemBuilder: (context, _, listViewIndex) {
                             final listViewTaskListRecord = _model
                                 .listViewPagingController!
--- lib/landing_view/task_to_do_page/task_to_do_page_model.dart
+++ lib/landing_view/task_to_do_page/task_to_do_page_model.dart
@@ -1,6 +1,7 @@
 import '/auth/firebase_auth/auth_util.dart';
 import '/backend/backend.dart';
 import '/backend/schema/structs/index.dart';
+import '/component/no_data_view/no_data_view_widget.dart';
 import '/customer_view/create_customer_view/create_customer_view_widget.dart';
 import '/flutter_flow/flutter_flow_theme.dart';
 import '/flutter_flow/flutter_flow_util.dart';
