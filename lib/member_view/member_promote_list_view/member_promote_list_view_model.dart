a252de0c3217d4f56991ed0d9065e5764c9ea9dc
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2024-09-16 07:50:50 +0000

    Updating to latest FlutterFlow output.

--- lib/member_view/member_promote_list_view/member_promote_list_view_model.dart
+++ lib/member_view/member_promote_list_view/member_promote_list_view_model.dart
@@ -6,6 +6,7 @@ import '/flutter_flow/flutter_flow_util.dart';
 import '/actions/actions.dart' as action_blocks;
 import '/custom_code/actions/index.dart' as actions;
 import 'member_promote_list_view_widget.dart' show MemberPromoteListViewWidget;
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/scheduler.dart';
 import 'package:google_fonts/google_fonts.dart';
