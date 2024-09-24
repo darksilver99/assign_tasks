408a0693b7bcdc329e8e7ac7de64cbb87a894cf9
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2024-09-24 10:03:38 +0000

    Updating to latest FlutterFlow output.

--- lib/task_view/own_task_name_view/own_task_name_view_model.dart
+++ lib/task_view/own_task_name_view/own_task_name_view_model.dart
@@ -1,6 +1,7 @@
 import '/backend/backend.dart';
 import '/flutter_flow/flutter_flow_theme.dart';
 import '/flutter_flow/flutter_flow_util.dart';
+import '/custom_code/actions/index.dart' as actions;
 import 'own_task_name_view_widget.dart' show OwnTaskNameViewWidget;
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
@@ -9,6 +10,17 @@ import 'package:google_fonts/google_fonts.dart';
 import 'package:provider/provider.dart';
 
 class OwnTaskNameViewModel extends FlutterFlowModel<OwnTaskNameViewWidget> {
+  ///  Local state fields for this component.
+
+  bool isMember = false;
+
+  bool isLoading = true;
+
+  ///  State fields for stateful widgets in this component.
+
+  // Stores action output result for [Custom Action - checkIsStillMember] action in OwnTaskNameView widget.
+  bool? isMemberResult;
+
   @override
   void initState(BuildContext context) {}
 
